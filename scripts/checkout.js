34f36af498f5740c590bfba4994198e966af1b9c
harshita <harshitamule95@gmail.com>
2024-06-09 23:06:55 +0530

    make delivery options(radio buttons) interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {calculateCartQuantity, cart, removeFromCart, updateQuantity} from '../data/cart.js';
+import {calculateCartQuantity, cart, removeFromCart, updateQuantity,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
@@ -98,7 +98,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += 
-      `<div class="delivery-option">
+      `<div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked? 'checked':''}
           class="delivery-option-input"
@@ -177,3 +179,13 @@ function updateCartQuantity(){
   document.querySelector('.js-update-checkout').innerHTML = `${cartQuantity} items`;
 }
 updateCartQuantity();
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    element.addEventListener('click', ()=>{
+      const productId = element.dataset.productId;
+      const deliveryOptionId = element.dataset.deliveryOptionId;
+
+      updateDeliveryOption(productId, deliveryOptionId)
+    });
+  });
